dd399d706c79112ae4ea5f7dd799b5b3d9826e1f
Emil Hammarstedt <emil.hammarstedt@gmail.com>
2017-05-09 20:27:06 +0200

    config script

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ cd ../frontend
 
 #vid dev kör igång docker med node som kör en utvecklingsserver
 #om man kör windows kan ska något liknande användas
-# winpty docker run -ti --name frontend --rm -p 3000:3000 -v "c:\Users\Emil\Documents\work\arbetsprov\frontend":/usr/src/app -w /usr/src/app node bash
+# winpty docker run -ti --name frontend --rm -p 3000:3000 -v "c:\Users\<username>\Documents\work\arbetsprov\frontend":/usr/src/app -w /usr/src/app node bash
 
 #i normal posix-miljö
 docker run -ti --name frontend --rm -p 3000:3000 -v $(pwd):/app -w /app --net=host node bash
